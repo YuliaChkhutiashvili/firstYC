6d984df3704ef5cde21e43dc752f279dd23f3599
Yulia <iulia.chkhutiashvili@gmail.com>
2025-03-03 09:10:05 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+
+//Homework1 (a)
+/*
+public class Main {
+    public static void main(String[] args) {
+    int firstNumber=8;
+    int secondNumber=7;
+    int thirdNumber=16;
+    int fourthNumber=9;
+    System.out.println(addFourNumbers(firstNumber,secondNumber,thirdNumber,fourthNumber));
+    }
+        static int addFourNumbers ( int a, int b, int c, int d) {
+            return (a+b+c+d)/4;
+        }
+    }
+*/
+//Homework1 (b)
+
+public class Main {
+  public static void main(String[] args) {
+    String name = "I want ";
+    String name2 = "to learn ";
+    String name3 = "Java";
+    int sumLenght = name.length()+name2.length()+name3.length();
+    System.out.println("The sum length of the name,name2,name3 strings is: " + sumLenght);
+
+
+  }
+}
+//შეკრება ვეღარ გავაკეთე, ვერ გავიგე როგორ :(
